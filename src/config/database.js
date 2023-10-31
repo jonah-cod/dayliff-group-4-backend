530bc6826ceab0f8a8530eaa1aaa1162d4997bed
Josphat Mwangi <josphatwanjiruw@gmail.com>
2023-10-31 09:09:31 +0300

    rename config to database

---
+++ src/config/database.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const connect = async () => {
+  // connect to DB
+  mongoose.set("strictQuery", false);
+  const mongoOptions = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  };
+  const mongodbUri = process.env.mongoDBUri;
+  await mongoose.connect(mongodbUri, mongoOptions);
+  const conn = mongoose.connection;
+  conn.on("error", console.error.bind(console, "connection error:"));
+};
+
+export default connect;
